0c83f382fb8a2aa36493665d78301bef2bda5012
Juan Pablo Urra <jp.urra42@gmail.com>
2025-08-20 18:37:03 -0400

    add more asserts and mockup with moqup

--- StringManipulation.Test/StringOperationsTest.cs
+++ StringManipulation.Test/StringOperationsTest.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
 
 namespace StringManipulation.Test
 {
@@ -16,7 +13,7 @@ namespace StringManipulation.Test
 
             //Act
             var result = strOperations.ConcatenateStrings("Hello", "Platzi");
-            
+
             //Assert
             //primero lo esperado y luego lo que retorna{
             Assert.NotNull(result);
@@ -55,5 +52,82 @@ namespace StringManipulation.Test
             //Assert
             Assert.Equal("JuanPablo", result);
         }
+
+        [Fact]
+        public void QuantintyInWords()
+        {
+            var strOperations = new StringOperations();
+            var result = strOperations.QuantintyInWords("Balloon", 1000);
+            Assert.StartsWith("mil", result);
+            Assert.Contains("Balloon", result);
+        }
+
+        [Fact]
+        public void GetStringLenth_Exception()
+        {
+            var strOperations = new StringOperations();
+            Assert.ThrowsAny<ArgumentNullException>(() => strOperations.GetStringLength(null));
+        }
+
+        [Fact]
+        public void GetStringLength_NoException()
+        {
+            var strOperations = new StringOperations();
+
+            var exception = Record.Exception(() => strOperations.GetStringLength("Hola"));
+
+            Assert.Null(exception);
+        }
+
+        [Theory]
+        [InlineData("V", 5)]
+        [InlineData("III", 3)]
+        [InlineData("X", 10)]
+        public void FromRomanToNumber(string romanNumber, int expected)
+        {
+            var strOperations = new StringOperations();
+
+            var result = strOperations.FromRomanToNumber(romanNumber);
+
+            Assert.Equal(expected, result);
+        }
+
+        //Skip
+
+        [Fact(Skip = "Esta prueba no es valida en este momento")]
+        public void ConcatenateStringsSkip()
+        {
+            //Arrange
+            var strOperations = new StringOperations();
+
+            //Act
+            var result = strOperations.ConcatenateStrings("Hello", "Platzi");
+
+            //Assert
+            //primero lo esperado y luego lo que retorna{
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal("Hello Platzi", result);
+        }
+
+        [Fact]
+        public void CountOccurrences()
+        {
+            //Arrange
+
+            //aqui se utiliza la libreria Moq
+            var mockLogger = new Mock<ILogger<StringOperations>>();
+
+            var strOperations = new StringOperations(mockLogger.Object);
+
+
+
+
+            //Act
+            var result = strOperations.CountOccurrences("Platzi", 'i');
+            //Assert
+            Assert.Equal(1, result);
+        }
+
     }
 }
